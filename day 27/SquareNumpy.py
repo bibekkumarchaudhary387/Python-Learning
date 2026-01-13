4d1e31e74e329b57f868dfc9c09ef0166cd3696a
bibekkumarchaudhary387 <bibekchaudhary2439@gmail.com>
2026-01-13 16:48:24 +0545

    square root numpy

---
+++ day 27/SquareNumpy.py
@@ -0,0 +1,9 @@
+import numpy as np
+
+#Square every element of an array
+
+#Find square root of numbers 1 to 10
+
+arr = np.array([1,2,3,4,5,6,7,8,9,10])
+
+print(np.sqrt(arr))
---
+++ day 27/numpyMathe.py
@@ -0,0 +1,13 @@
+import numpy as np
+
+arr1 = np.array([
+    [1,2],
+    [3,4]
+])
+
+arr2 = np.array([
+    [5,6],
+    [7,8]
+])
+
+print(np.sin(arr1))
