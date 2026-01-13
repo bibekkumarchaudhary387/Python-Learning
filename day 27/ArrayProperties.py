94122b18fde5858ecf91df4b7120ee026ca25f38
bibekkumarchaudhary387 <bibekchaudhary2439@gmail.com>
2026-01-13 16:11:50 +0545

    Create a 3D array and print its shape and dimensions

---
+++ day 27/ArrayProperties.py
@@ -0,0 +1,11 @@
+import numpy as np
+
+array = np.array([[1,2,3],[4,5,6]])
+
+print(array.size) #matrix size
+
+print(array.ndim) #dimension of array
+
+print(array.shape) #array size
+
+print(array.dtype) #datatype
---
+++ day 27/array3d.py
@@ -0,0 +1,14 @@
+
+#Create a 3D array and print its shape and dimensions
+import numpy as np
+
+array3d = np.array([
+    [[1,2],[3,4]],
+    [[5,6],[7,8]]
+    ])
+
+print(array3d.size)
+
+print(array3d.ndim)
+
+print(array3d.shape)
