b0ef4c75c0fc5d9a9f60670701f9086a73d10d2b
bibekkumarchaudhary387 <bibekchaudhary2439@gmail.com>
2026-01-14 18:43:21 +0545

    Add [100, 200, 300] to each row of a 2D array

---
+++ day 27/Broadcastingnumpy.py
@@ -0,0 +1,15 @@
+import numpy as np
+
+# #1d array broadcasting
+# arr1d = np.array([1,2,3,4])
+# arrb = 2
+
+# print(arr1d + arrb)
+
+#2d array broadcasting
+a = np.array([[1,2,3],[4,5,6]])
+b = np.array([10,20,30])
+
+sum = a+b
+
+print(sum)
---
+++ day 27/broadcasting2darray.py
@@ -0,0 +1,8 @@
+#Add [100, 200, 300] to each row of a 2D array
+
+import numpy as np
+
+a = np.array([[1,2,3],[4,5,6]])
+b = np.array([100,200,300])
+
+print(a+b)
